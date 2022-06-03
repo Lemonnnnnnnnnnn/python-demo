9684bcf5b7dd3eb94b66babc6f22d71d4dd21a59
lemon <1041707577@qq.com>
2022-06-03 10:27:32 +0800

    feat: decorator

--- decorator.py
+++ decorator.py
@@ -1,9 +1,13 @@
-def log(func):
-    print('call' , func.__name__) # 打印函数名
-    return lambda *args , **kwargs : func(*args , **kwargs)
+def log(user , operate):
+    def decorator(func):
+        print(user, operate , func.__name__) # 打印函数名
+        return lambda *args , **kwargs : func(*args , **kwargs)
+    return decorator
 
-@log
+@log('root' , 'call')
 def hello(name):
     print('hello' ,name)
 
-hello('liming') # call hello , hello liming
+hello('liming') # root call hello hello liming
+
+
