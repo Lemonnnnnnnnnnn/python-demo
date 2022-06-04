6360acefdc1b22e73a012067002530384d8d6d22
lemon <1041707577@qq.com>
2022-06-04 11:47:08 +0800

    feat: 面向对象

--- module/hello.py
+++ module/hello.py
@@ -1,5 +1,15 @@
+import sys
+
+
 def func():
-    print(__name__)
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, world!')
+    elif len(args) == 2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
 
 if __name__ == "__main__":
-    func() # __main__
+    func()  # __main__
---
+++ object_oriented.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self , name , age) :
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        print(self.__name) 
+
+class Monitor(Student):
+    # def __init__(self , name , age) :
+    #     super().__init__(name , age) # 调用父类的构造器
+       
+
+    def __init__(self , name , age) :
+        print(self)
+        self.__name = name
+        self.__age = age
+
+
+bart = Monitor('bart' , 18 )
+
+bart.get_name()
